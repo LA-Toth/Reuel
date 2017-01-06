8e501f63bb4d4c7a63f6c9e5e1eee037fffb525d
Laszlo Attila Toth <laszlo.attila.toth+reuel@gmail.com>
2017-01-06 09:03:51 +0100

    x

--- reuel/config.rb
+++ reuel/config.rb
@@ -1,7 +1,7 @@
-# Copyright 2016-2017 Laszlo Attila Toth
-# Distributed under the terms of the GNU General Public License v3
-
 module Reuel
+  #class EntryNotFound < KeyError
+  #	nil
+  #end
 
   class Config
 
@@ -13,11 +13,54 @@ module Reuel
 
     attr_reader :config
 
-    # @param [String] entry
+    # @param [String] entry The entry name in config tree, eg. commands.splitlog.delimiter
+    # @param [Object] value The value of the entry, it can have any type, eg. ':', or 3.14
     def set(entry, value)
       c, key = get_container_and_key(entry)
-      c[key] = value
+
+      if c.include?(key) and key.instance_of?(Hash)
+
+	c[key] = value
+      end
     end
+
+    # @param [String] entry The entry name in config tree, eg. commands.splitlog.delimiter, which represents a list
+    # @param [Object] value The value of the entry, it can have any type, eg. ':', or 3.14
+    def append(entry, value)
+      c, key = get_container_and_key(entry)
+
+      c[key] = [] unless c.include?(key)
+      c[key] << value
+    end
+
+    # @param [String] entry The entry name in config tree, eg. commands.splitlog.delimiter, which represents a set
+    # @param [Object] value The value of the entry, it can have any type, eg. ':', or 3.14
+    def add_to_set(entry, value)
+      c, key = get_container_and_key(entry)
+
+      c[key] = Set.new unless c.include?(key)
+      c[key] << value
+    end
+
+    # @param [String] entry The entry name in config tree, eg. commands.splitlog.delimiter
+    def get(entry)
+      puts entry
+      keys = entry.split('.')
+      puts keys
+      puts @config
+      c = @config
+      puts c
+      return c
+
+      begin
+	keys.each { |key| c = c[key] }
+	c = c[key]
+      rescue KeyError
+	return @config.default
+      end
+      c
+    end
+
 
     protected
 
